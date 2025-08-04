c89665314dd2b911b10ce5da3be95c3344cb5b3a
Y M Horn <ymh53r@gmail.com>
2025-08-04 17:04:06 +0300

    Created final dictionary

---
+++ src/dataClean/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from capitalWords import CapitalAmount
+from commonWords import CommonWords
+from longestTweets import LongestTweets
+from tweetAmounts import TweetAmounts
+from tweetLengths import TweetLengths
+from pprint import pprint
+
+df = pd.read_csv('C:\\Users\\Yisroel Meir\\PycharmProjects\\Antisemitic_tweets\\data\\tweets_dataset.csv')
+final_dict = {}
+column = 'Biased'
+word_column = 'Text'
+df[column].replace(0,'non_antisemitic',inplace=True)
+df[column].replace(1,'antisemitic',inplace=True)
+capital = CapitalAmount(df)
+common = CommonWords(df)
+longest = LongestTweets(df)
+amounts = TweetAmounts(df)
+length = TweetLengths(df)
+final_dict['total_tweets'] = amounts.amounts(column)
+final_dict['average_length'] = length.average(column, word_column)
+final_dict['common_words'] = common.common(word_column)
+final_dict['longest_3_tweets'] = longest.longest(column, word_column)
+final_dict['uppercase_words'] = capital.capital_for_all(column, word_column)
+pprint(final_dict)
