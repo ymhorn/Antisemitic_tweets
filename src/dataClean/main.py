e4cbbd336ff0ab92a6ddb21872a2ab79b52b1969
Y M Horn <ymh53r@gmail.com>
2025-08-04 17:08:46 +0300

    Added json

--- src/dataClean/main.py
+++ src/dataClean/main.py
@@ -5,6 +5,7 @@ from longestTweets import LongestTweets
 from tweetAmounts import TweetAmounts
 from tweetLengths import TweetLengths
 from pprint import pprint
+import json
 
 df = pd.read_csv('C:\\Users\\Yisroel Meir\\PycharmProjects\\Antisemitic_tweets\\data\\tweets_dataset.csv')
 final_dict = {}
@@ -23,3 +24,7 @@ final_dict['common_words'] = common.common(word_column)
 final_dict['longest_3_tweets'] = longest.longest(column, word_column)
 final_dict['uppercase_words'] = capital.capital_for_all(column, word_column)
 pprint(final_dict)
+
+with open('C:\\Users\\Yisroel Meir\\PycharmProjects\\Antisemitic_tweets\\results\\results.json','w') as f:
+    json.dump(final_dict,f)
+
